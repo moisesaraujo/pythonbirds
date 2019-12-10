6157890429a1f272ad1e7dfb677f8526d023bc3b
Moises araujo <moises@moisesaraujo.com>
2019-12-10 10:43:06 -0300

    Incluisão Pacote oo e class Pessoa atributos criado atributos complexos filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,8 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=48):
-        self.nome=nome
-        self.idade=idade
+    def __init__(self,*filhos, nome=None, idade=48):
+        self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
 
     def cuplimentar(self):
         return f'Ola!'
@@ -9,7 +10,15 @@ class Pessoa:
 if __name__ == "__main__":
     p = Pessoa()
     p.nome="moises"
+    miria = Pessoa(nome="Miria")
+    larissa = Pessoa(nome="Larissa")
+    moises = Pessoa(miria,larissa,nome="Moises")
+
+    #moises = Pessoa(larissa,nome="Larissa")
     p.idade==49
     print(p.cuplimentar())
     print(f"nome {p.nome}, idade {p.idade}")
+
+    for filho in moises.filhos:
+        print(filho.nome)
 
