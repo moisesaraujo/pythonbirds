066b79a8366ed71bdd15f3ea410cfb39d83947ac
Moises araujo <moises@moisesaraujo.com>
2019-12-10 10:03:09 -0300

    Incluisão Pacote oo e class Pessoa atributos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,15 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=48):
+        self.nome=nome
+        self.idade=idade
+
+    def cuplimentar(self):
+        return f'Ola!'
+
+if __name__ == "__main__":
+    p = Pessoa()
+    p.nome="moises"
+    p.idade==49
+    print(p.cuplimentar())
+    print(f"nome {p.nome}, idade {p.idade}")
+
